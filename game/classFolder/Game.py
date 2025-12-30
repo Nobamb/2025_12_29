ad6b912f326a023e75488dd09e7fd9a8e68a2625
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2025-12-30 16:56:54 +0900

    game 클래스 상속화

--- game/classFolder/Game.py
+++ game/classFolder/Game.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 
 # Game 클래스 지정
-class Game:
+class Game(tk.Tk):
   def __init__(self,width, height, title, geometry):
     # tk가져오기(게임 시작)
-    self.game = tk.Tk()
+    # 상속받기
+    super().__init__()
 
     # 사이즈조절 가능 여부 설정
     # 둘다 조절 못하게 설정
-    self.game.resizable(width, height)
-
-
+    self.resizable(width, height)
+    
     # 제목 똥피하기 게임
     # 크기 1000(가로) 800(세로)
-    self.game.title(title)
+    self.title(title)
 
-    self.game.geometry(geometry)
+    self.geometry(geometry)
