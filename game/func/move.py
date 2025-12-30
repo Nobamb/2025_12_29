5c7fd69958a05b8f08eb6caabedbf141db1dbc8e
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2025-12-30 18:03:37 +0900

    움직임 함수 move 생성

---
+++ game/func/move.py
@@ -0,0 +1,15 @@
+# # 왼쪽움직이게 지정
+def move(event, value, object_place=player.object_place):
+    # # 왼쪽으로 움직일 것이기에 player_x를 감소시킨다
+    # global Object_place
+    # 10만큼 감소
+    object_place.x += value
+    # 동작 테스트
+    print("왼쪽이동", object_place.x)
+    # 플레이어 위치 재설정
+    return object_place(x=object_place.x, y=object_place.y)
+
+
+# # 왼쪽움직이게 지정
+def move_left(e):
+    return move(e, -10)
