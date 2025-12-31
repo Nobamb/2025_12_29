94f949173668b6d7038c85e4ebc4c7075a61f1c7
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2025-12-31 17:00:22 +0900

    초기 코드 작성(똥내려올 때 최적화)

--- game/func/dung_down.py
+++ game/func/dung_down.py
@@ -22,10 +22,14 @@ def dung_down():
     if Game_State.is_game_over:
         return
     # dung_list의 값들을 모두 가져온다.
-    for dung in dung_list:      
+    for dung_index,dung in enumerate(dung_list):      
       # 전역변수 dung_place를 가져옴
       dung.object_place.y += 10
       dung.place(x=dung.object_place.x, y=dung.object_place.y)
+      # 만약에 dung의 y 좌표값이 800이상이라면?
+      # 그 값을 제거하기(게임 최적화 목적)
+      if(dung.object_place.y > 800):
+        dung_list.pop(dung_index)
     # 만약에 is_game_over이 false면?(즉, 아직 게임오버가 안된 상황이면?)
     if not Game_State.is_game_over:
         # after 함수를 사용하여 0.5(50ms)초마다 재귀 동작을 하도록 함
