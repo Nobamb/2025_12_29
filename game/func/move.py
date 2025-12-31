747046561d0d11c7dee0102217747127e6705b97
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2025-12-31 15:22:03 +0900

    move 함수 수정

--- game/func/move.py
+++ game/func/move.py
@@ -1,4 +1,6 @@
-# # 왼쪽움직이게 지정
+from player import player
+
+# # 움직이게 지정
 def move(event, value, object_place=player.object_place):
     # # 왼쪽으로 움직일 것이기에 player_x를 감소시킨다
     # global Object_place
@@ -7,9 +9,5 @@ def move(event, value, object_place=player.object_place):
     # 동작 테스트
     print("왼쪽이동", object_place.x)
     # 플레이어 위치 재설정
-    return object_place(x=object_place.x, y=object_place.y)
+    return player.place(x=object_place.x, y=object_place.y)
 
-
-# # 왼쪽움직이게 지정
-def move_left(e):
-    return move(e, -10)
