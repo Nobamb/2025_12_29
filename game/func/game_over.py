8da7eadd50af0e2666ee0d85accc099c86dec448
nobamb <jsh12345644[@gmail.com](mailto:you@example.com)>
2025-12-31 16:46:52 +0900

    무한한 똥들이 닿으면 게임 오버되도록

--- game/func/game_over.py
+++ game/func/game_over.py
@@ -1,6 +1,6 @@
 # 오브젝트 가져오기
 from data.player import player
-from data.dung import dung
+from func.make_dung import dung_list
 # dung_bool 가져오기
 from data.Game_State import Game_State
 # 버튼 가져오기
@@ -17,13 +17,15 @@ from game_over_button.re_button import re_button
 def game_over():
     # dung_bool 가져오기
     global Game_State
-    if (
-        player.object_place.y <= dung.object_place.y + 20 and player.object_place.y >= dung.object_place.y - 20
-    ) and (player.object_place.x <= dung.object_place.x + 20 and player.object_place.x >= dung.object_place.x - 20):
-        print("게임 오버")
-        # is_game_over true값 변경
-        Game_State.is_game_over = True
-        # 다시하기 버튼 생성
-        re_button.pack()
-        # 끝내기 버튼 생성
-        finish_button.pack()  
+    # 모든 dung에 대해 지정
+    for dung in dung_list:    
+      if (
+          player.object_place.y <= dung.object_place.y + 20 and player.object_place.y >= dung.object_place.y - 20
+      ) and (player.object_place.x <= dung.object_place.x + 20 and player.object_place.x >= dung.object_place.x - 20):
+          print("게임 오버")
+          # is_game_over true값 변경
+          Game_State.is_game_over = True
+          # 다시하기 버튼 생성
+          re_button.pack()
+          # 끝내기 버튼 생성
+          finish_button.pack()  
